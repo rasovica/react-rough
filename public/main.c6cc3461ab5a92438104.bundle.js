4ce29df87cbbc8b2626b563bd5cc4dfd09d5a25f
Raslav Milutinovic <admin@rasovica.com>
2023-03-14 23:55:02 +0100

    feat: try to fix package

---
+++ public/main.c6cc3461ab5a92438104.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1267:function(n,o,c){"use strict";c.r(o);c(1268),c(1279)},465:function(n,o,c){c(466),c(609),n.exports=c(1267)},528:function(n,o){}},[[465,1,2]]]);
